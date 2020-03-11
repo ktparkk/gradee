fdfbfd0a1fc217ab1148a04f1b22293eab833d6f
Katie Park <katiepark@Katies-MacBook-Pro.local>
2020-03-10 23:46:09 -0400

    semester structure

--- main.py
+++ main.py
@@ -3,40 +3,40 @@ from func import *
 
 if argUnique("-n"): # create new semester
     # add new semester
-    print("\nNew unique semester name (e.g. Spring2020)")
+    print("\nNew unique semester name (e.g. Spring2020): ", end =" ")
     semesterName = input()
     while newSemester(semesterName) == False:
-        print("\nSemester name already exists; please retry: ")
+        print("\nSemester name already exists; please retry: ", end =" ")
         semesterName = input()
     semester_csv = open(newSemester(semesterName), 'w')
     semester_writer = csv.writer(semester_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-    # add courses to semester
     courseNum = 1
-    print("Name of " + ordinalNum(courseNum) + " course: ")
-    courseName = input()
-    courseRow = [courseName]
+    while True: # add courses to semester
+        print("\nName of " + ordinalNum(courseNum) + " course: ", end =" ")
+        courseName = input()
+        courseRow = [courseName]
 
-    assessNum = 1
-    assessValueRow = ["Value"]
-    while True: # add assessment types to course
-        print("Name of " + ordinalNum(assessNum) + " assessment in " + courseName + ": ")
-        courseRow.append(input())
-        print("Value of " + ordinalNum(assessNum) + " assessment in " + courseName + ": ")
-        assessValueRow.append(input())
-        print("Add another assessment to " + courseName + "? (y/n): ")
+        assessNum = 1
+        assessValueRow = ["Value"]
+        while True: # add assessment types to course
+            print("\nName of " + ordinalNum(assessNum) + " assessment in " + courseName + ": ", end =" ")
+            courseRow.append(input())
+            print("Value of " + ordinalNum(assessNum) + " assessment in " + courseName + ": ", end =" ")
+            assessValueRow.append(input())
+            print("\nAdd another assessment to " + courseName + "? (y/n): ", end =" ")
+            answer = input()
+            if answer != "y": break
+            assessNum += 1
+        semester_writer.writerow(courseRow)
+        semester_writer.writerow(assessValueRow)
+
+        print("\nAdd another course to " + semesterName + "? (y/n): ", end =" ")
         answer = input()
         if answer != "y": break
-        assessNum += 1
-        
-    semester_writer.writerow(courseRow)
-    semester_writer.writerow(assessValueRow)
-
-
-
-
-
-
+        courseNum += 1
 
     semester_writer.writerow(["Course","Assessment","Name","Grade","Date","Notes"]) # add fields for submission input(column headers)
-    
+
+if argUnique("-a"): # add submission
+    print(listSemesters())
